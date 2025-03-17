9d48b2ede3badac831add9ed9f68c108980d0033
MiguelQuisay <159466069+MiguelQuisay@users.noreply.github.com>
2025-03-17 10:40:42 +0800

    finish social media app

--- projects/CRUD PROJECT/main.js
+++ projects/CRUD PROJECT/main.js
@@ -26,4 +26,27 @@ form.addEventListener("submit", (e) => {
   let acceptData = () => {
     data["text"] = input.value;
     console.log(data);
+    createPost();
+  };
+
+  let createPost = () => {
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+  };
+
+  let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+  };
+
+  let editPost = (e) => {
+    input.value = e.parentElement.previousElementSibling.innerHTML;
+    e.parentElement.parentElement.remove();
   };
